8ec27d86eb8d02f64bc6e1d10f3268bbe8c89319
Oriol Burgaya <oriol.burgaya@celeramotion.com>
2020-02-07 14:32:14 +0100

    Game grid generated

--- src/main.c
+++ src/main.c
@@ -17,6 +17,13 @@ int main(void)
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
+            // Generation of the game grid
+            for(i = 0; i < GRID_V_CELLS_NUMBER; ++i)
+            {
+                for(j = 0; j < GRID_H_CELLS_NUMBER; ++j) {
+                    DrawRectangleLines(j*GRID_CELL_WIDTH, i*GRID_CELL_HEIGHT, GRID_CELL_WIDTH, GRID_CELL_HEIGHT, BLACK);
+                }
+            }
             DrawRectangleLines(0, 0, GRID_CELL_WIDTH, GRID_CELL_HEIGHT, BLACK);
 
             DrawText("Let's play ray Tetris!", 190, 200, 20, LIGHTGRAY);
